c65cbae5618c169ebe9e0dfd7c7078de5c007149
sherif Abdelrhman <sellkady038@gmail.com>
2024-03-26 06:33:31 +0200

    Create loading layout. Create showLoading and hideLoading.

--- app/src/main/java/com/mis/route/chatapp/base/BaseFragment.kt
+++ app/src/main/java/com/mis/route/chatapp/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.mis.route.chatapp.base
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,11 +9,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
+import com.mis.route.chatapp.R
 
 abstract class BaseFragment<VM:ViewModel,DB:ViewDataBinding> : Fragment() {
 
     lateinit var viewModel : VM
     lateinit var dataBinding : DB
+    private var dialog : AlertDialog?=null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +33,22 @@ abstract class BaseFragment<VM:ViewModel,DB:ViewDataBinding> : Fragment() {
     abstract fun initViewModel() : VM
 
     fun showLoading(){
-
+        val builder = AlertDialog.Builder(activity)
+        builder.setView(R.layout.loading)
+        dialog = builder.create()
     }
     fun hideLoading(){
-
+        dialog?.dismiss()
     }
 
+    fun showDialog(
+        title : String?=null,
+        message : String?=null,
+        posBtnTitle : String?=null,
+        negBtnTitle : String?=null,
+
+    ){
+        dialog?.setTitle(title)
+        dialog?.setMessage(message)
+    }
 }
